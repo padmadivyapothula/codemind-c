c5cd72fac8113c5ba606b2b04b08a5cef8267764
padmadivyapothula <119720502+padmadivyapothula@users.noreply.github.com>
2022-12-03 20:14:03 +0530

    #include<stdio.h> int main() {     int a,b,x;     scanf("%d%d",&a,&b);     x=a/b;     if(x>=30)     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Expenditure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x;
+    scanf("%d%d",&a,&b);
+    x=a/b;
+    if(x>=30)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
